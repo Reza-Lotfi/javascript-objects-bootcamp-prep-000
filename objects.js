e8ba700e5340afa23321c8a93ddb1366906c4ac5
Reza Lotfi <rezaiscool7@aol.com>
2018-08-29 21:38:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {artist: 'song'}
 
+<<<<<<< HEAD
 function updatePlaylist(playlist, artist, song){
   playlist[artist] = song
   return playlist
@@ -8,4 +9,9 @@ function updatePlaylist(playlist, artist, song){
 function removeFromPlaylist (playlist, artistName){
   delete playlist[artistName]
   return playlist
+=======
+function updatePLaylist(playlist, artist, song) {
+  playlist[artist] = song
+  return playlist
+>>>>>>> a7a6a2ffde438299020fd807f6abb63b487910a7
 }
